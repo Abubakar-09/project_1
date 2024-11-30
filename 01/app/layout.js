42e6a491ccf9dbfee89f426809646c56a57e597a
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-11-30 22:25:38 +0500

    Update layout.js

--- 01/app/layout.js
+++ 01/app/layout.js
@@ -10,9 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head>
-    <link rel="icon" type="image/svg+xml" href="/logo.svg">
-    </head>
       <body className="bg-black">
         <Navbar />
         {/* Main Content */}
