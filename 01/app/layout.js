efd406a8179aa1ae4c411ef90dfbcac293d44538
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-11-30 20:16:47 +0500

    Update layout.js

--- 01/app/layout.js
+++ 01/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <head>
-    <link rel="icon" type="image/x-icon" href="/logo.svg">
+    <link rel="icon" type="image/svg+xml" href="/logo.svg">
     </head>
       <body className="bg-black">
         <Navbar />
