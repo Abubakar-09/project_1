5576a3cf143d4443bb8e7934b4a3d3dd274ebaaf
AbuBakar Chohan <132102392+Abubakar-09@users.noreply.github.com>
2024-12-08 21:04:33 +0500

    Add files via upload

---
+++ 02/app/faqs/page.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <>
+            <section class="text-gray-600 w-[80vw] m-auto max-md:w-[90vw] body-font">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+                        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Questions Looks Here</h1>
+                        <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero vitae aliquid ut architecto eum eligendi!</p>
+                    </div>
+                    <div class="flex flex-wrap -m-4">
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">How we serve food?</h2>
+                                <p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime recusandae animi molestiae commodi ex quae beatae, laboriosam porro temporibus eligendi..</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">How we serve food?</h2>
+                                <p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime recusandae animi molestiae commodi ex quae beatae, laboriosam porro temporibus eligendi..</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">How we serve food?</h2>
+                                <p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime recusandae animi molestiae commodi ex quae beatae, laboriosam porro temporibus eligendi..</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">How we serve food?</h2>
+                                <p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime recusandae animi molestiae commodi ex quae beatae, laboriosam porro temporibus eligendi..</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">How we serve food?</h2>
+                                <p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime recusandae animi molestiae commodi ex quae beatae, laboriosam porro temporibus eligendi..</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                               
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">How we serve food?</h2>
+                                <p class="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime recusandae animi molestiae commodi ex quae beatae, laboriosam porro temporibus eligendi..</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default page
